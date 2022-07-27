4f49bdcdb788d44e6324872f8f413136051f508b
Nik Light <russianik44@gmail.com>
2022-07-27 17:03:14 +0300

    Base of twitter project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <post-form @create="createPost"/>
+  <post-list v-bind:posts="posts" @addLike="addLike" @addDislike="addDislike"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import PostForm from "@/components/PostForm";
+  import PostList from "@/components/PostList";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      PostForm,
+      PostList
+    },
+    data(){
+      return {
+        posts: [
+          {id: 1, title: '1984', body: 'Romane of George Orwell', likes: 1984, dislikes: 0},
+          {id: 2, title: 'Hobbit', body: 'One of the greatest book in history', likes: 13450, dislikes: 0}
+        ],
+      }
+    },
+    methods: {
+      createPost(post) {
+        this.posts.push(post)
+      },
+
+      addLike(post) {
+        this.posts.forEach(element => {
+          if (element.id == post.id) {
+            element.likes++;
+          }
+        })
+      },
+
+      addDislike(post) {
+        this.posts.forEach(element => {
+          if (element.id == post.id) {
+            element.dislikes++;
+          }
+        })
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
